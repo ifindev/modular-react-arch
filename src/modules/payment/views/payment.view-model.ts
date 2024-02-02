d71a67b5cec9262e2a501187af8be08f2560dd37
Muhammad Arifin <muhammad.arifin@nri.co.id>
2024-02-02 23:06:06 +0700

    feat: refactor payment component to payment view

--- src/modules/payment/views/payment.view-model.ts
+++ src/modules/payment/views/payment.view-model.ts
@@ -1,10 +1,23 @@
 import { PaymentStrategAustralia } from '../domain/payment-strategy/payment-strategy-australia.use-case';
+import useDonation from '../hooks/use-donation';
 import usePaymentMethods from '../hooks/use-payment-methods';
 
 export default function usePaymentViewModel() {
   const amount = 19.3;
-  const paymentStrategy = new PaymentStrategAustralia();
-  const { paymentMethods } = usePaymentMethods();
+  const strategy = new PaymentStrategAustralia();
 
-  return { amount, paymentMethods, paymentStrategy };
+  const { paymentMethods } = usePaymentMethods();
+  const { total, donation, agreeToDonate, updateAgreeToDonate } = useDonation({
+    amount,
+    strategy,
+  });
+
+  return {
+    paymentMethods,
+    strategy,
+    total,
+    donation,
+    agreeToDonate,
+    updateAgreeToDonate,
+  };
 }
--- src/modules/payment/views/payment.view.tsx
+++ src/modules/payment/views/payment.view.tsx
@@ -1,14 +1,35 @@
-import Payment from '../components/payment';
+import DonationCheckbox from '../components/donation-checkbox';
+import PaymentMethods from '../components/payment-methods';
+import { formatDonationMessage } from '../utils/formatter.utill';
 import usePaymentViewModel from './payment.view-model';
 
 export default function PaymentView() {
-  const { amount, paymentMethods, paymentStrategy } = usePaymentViewModel();
+  const {
+    paymentMethods,
+    strategy,
+    total,
+    donation,
+    agreeToDonate,
+    updateAgreeToDonate,
+  } = usePaymentViewModel();
 
   return (
-    <Payment
-      amount={amount}
-      paymentMethods={paymentMethods}
-      strategy={paymentStrategy}
-    />
+    <div className="flex flex-col gap-2">
+      <h3 className="font-bold text-lg">Payment</h3>
+      <div className="flex flex-col">
+        <PaymentMethods paymentMethods={paymentMethods} />
+      </div>
+      <DonationCheckbox
+        onChange={updateAgreeToDonate}
+        checked={agreeToDonate}
+        label={formatDonationMessage({ donation, agreeToDonate, strategy })}
+      />
+      <div>
+        <button className="bg-red-600 text-white font-bold py-1 px-3 rounded">
+          {strategy.getCurrencySign()}
+          {total}
+        </button>
+      </div>
+    </div>
   );
 }
