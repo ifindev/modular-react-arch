ea60f90f1544d1a139a66600e68782ca9d964297
Muhammad Arifin <muhammad.arifin@nri.co.id>
2024-02-02 17:41:33 +0700

    feat: add donation checkbox

--- src/modules/payment/components/payment.tsx
+++ src/modules/payment/components/payment.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import { PaymentMethod } from '../domain/payment-method.model';
 import PaymentMethods from './payment-methods';
 
@@ -7,15 +8,37 @@ type Props = {
 };
 
 export default function Payment({ amount, paymentMethods }: Props) {
+  const [agreeToDonate, setAgreeToDonate] = useState(false);
+
+  const { total, tip } = useMemo(
+    () => ({
+      total: agreeToDonate ? Math.floor(amount + 1) : amount,
+      tip: parseFloat((Math.floor(amount + 1) - amount).toPrecision(10)),
+    }),
+    [agreeToDonate, amount]
+  );
+
   return (
     <div className="flex flex-col gap-2">
       <h3 className="font-bold text-lg">Payment</h3>
       <div className="flex flex-col">
         <PaymentMethods paymentMethods={paymentMethods} />
       </div>
+      <label htmlFor="donation" className="flex items-center gap-3">
+        <input
+          type="checkbox"
+          onChange={() => setAgreeToDonate((prev) => !prev)}
+          checked={agreeToDonate}
+        />
+        <p>
+          {agreeToDonate
+            ? 'Thanks for your donation.'
+            : `I would like to donate $${tip} to charity.`}
+        </p>
+      </label>
       <div>
         <button className="bg-red-600 text-white font-bold py-1 px-3 rounded">
-          ${amount}
+          ${total}
         </button>
       </div>
     </div>
--- src/modules/payment/views/payment.view-model.ts
+++ src/modules/payment/views/payment.view-model.ts
@@ -1,7 +1,7 @@
 import usePaymentMethods from '../hooks/use-payment-methods';
 
 export default function usePaymentViewModel() {
-  const amount = 100;
+  const amount = 19.3;
   const { paymentMethods } = usePaymentMethods();
 
   return { amount, paymentMethods };
