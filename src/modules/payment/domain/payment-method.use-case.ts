9df9185dade66f6f24c163d533c97e74e73db4b5
Muhammad Arifin <muhammad.arifin@nri.co.id>
2024-02-02 16:43:02 +0700

    feat: add payment-method usecase

---
+++ src/modules/payment/domain/payment-method.use-case.ts
@@ -0,0 +1,25 @@
+import { getRemotePaymentMethods } from '../../../apis/api';
+import { PaymentMethod } from './payment-method.model';
+import { RemotePaymentMethod } from './payment.type';
+
+const payInCash = new PaymentMethod({ name: 'cash' });
+
+export function convertPaymentMethods(
+  methods: RemotePaymentMethod[]
+): PaymentMethod[] {
+  if (methods.length === 0) {
+    return [];
+  }
+
+  const mapped: PaymentMethod[] = methods.map(
+    (method) => new PaymentMethod(method)
+  );
+  mapped.push(payInCash);
+
+  return mapped;
+}
+
+export async function fetchPaymentMethods(): Promise<PaymentMethod[]> {
+  const response = await getRemotePaymentMethods();
+  return convertPaymentMethods(response);
+}
